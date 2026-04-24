93df3bb1bfea4afd3e8d5545805d434d0eed354d
Rainer Simon <rainer@rainersimon.io>
2026-04-24 13:32:44 +0200

    Added react-router for future use

--- src/index.tsx
+++ src/index.tsx
@@ -1,10 +1,15 @@
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
+import { HashRouter, Route, Routes } from 'react-router';
 
 import './index.css';
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <div>Hello World</div>
+    <HashRouter>
+      <Routes>
+        <Route path="/" element={<div>Hello World</div>} />
+      </Routes>
+    </HashRouter>
   </StrictMode>,
 )
